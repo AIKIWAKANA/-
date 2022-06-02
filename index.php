fab30d57665d4b78fe534c413f6684a66ae90c12
AIKI WAKANA <aiki.wakana@gmail.com>
2022-06-02 09:07:30 +0900

    商品追加

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
-    $product = [
+    $productA = [
         "name" => "アボカド",
         "price" => 230,
-        "image" => "https://dnbz0c2oupsw6.cloudfront.net/bcekt8ctzrsfdj1gsus49v9tnhqu"
+        "image" => "./images/image_1.jpg"
+    ];
+
+    $productB = [
+        "name" => "オレンジ",
+        "price" => 100,
+        "image" => "./images/image_2.jpg"
+    ];
+
+    $productC = [
+        "name" => "レモン",
+        "price" => 200,
+        "image" => "./images/image_3.jpg"
+    ];
+
+    $productD = [
+        "name" => "パイナップル",
+        "price" => 450,
+        "image" => "./images/image_4.jpg"
     ];
 
 ?>
@@ -21,14 +39,42 @@
       <h1 class="title">DailyTrial Shopping</h1>
       <div class="cards-container">
         <div class="card">
-          <img class="card-image" src="<?php echo $product["image"]; ?>" alt="">
-          <p class="card-title"><?php echo $product["name"]; ?></p>
+          <img class="card-image" src="<?php echo $productA["image"]; ?>" alt="">
+          <p class="card-title"><?php echo $productA["name"]; ?></p>
           <div class="flex justify-between">
-            <p class="card-price"><?php echo $product["price"]."円"; ?></p>
+            <p class="card-price"><?php echo $productA["price"]."円"; ?></p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+
+        <div class="card">
+          <img class="card-image" src="<?php echo $productB["image"]; ?>" alt="">
+          <p class="card-title"><?php echo $productB["name"]; ?></p>
+          <div class="flex justify-between">
+            <p class="card-price"><?php echo $productB["price"]."円"; ?></p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+
+        <div class="card">
+          <img class="card-image" src="<?php echo $productC["image"]; ?>" alt="">
+          <p class="card-title"><?php echo $productC["name"]; ?></p>
+          <div class="flex justify-between">
+            <p class="card-price"><?php echo $productC["price"]."円"; ?></p>
+            <input min="0" class="item-number" type="number" value="0">
+          </div>
+        </div>
+
+        <div class="card">
+          <img class="card-image" src="<?php echo $productD["image"]; ?>" alt="">
+          <p class="card-title"><?php echo $productD["name"]; ?></p>
+          <div class="flex justify-between">
+            <p class="card-price"><?php echo $productD["price"]."円"; ?></p>
             <input min="0" class="item-number" type="number" value="0">
           </div>
         </div>
       </div>
+      
       <div class="btn-footer bg-white">
         <input class="cart-btn" type="submit" name="submit" value="カートに追加" />
       </div>
